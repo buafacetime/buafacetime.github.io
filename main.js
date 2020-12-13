6bd16713812214ef2795ba4a13f10ee42b7c3076
buafacetime <70013749+buafacetime@users.noreply.github.com>
2020-12-13 13:48:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,70 +1,74 @@
-
-const name = prompt('Enter room name').trim();
-console.log(name);
-const exampSocket = new WebSocket('ws://localhost:8080', 'echo-protocol');
-
-exampSocket.onopen = (e) => {
-	exampSocket.send(JSON.stringify({
-		type:"enter_room",
-		name: "jive"
-	})); //room name
-};
-
-exampSocket.onmessage = (e) => {
-	console.log('you got message....');
-
-	const data = JSON.parse(e.data);
-
-	console.log(data);
-	const clientMsg = (data) => {
-		let msg = document.createElement('div');
-		msg.textContent = `from :: ${data.id} \n ${data.text}`;
-		document.querySelector('#msgs').appendChild(msg);
-		return;
-	};
-
-	const serverMessage = (text) => {
-		const msg = document.createElement('div');
-		msg.textContent = `from server:: ${text}`;
-		document.querySelector('#msgs').appendChild(msg);
-		return;
-	};
-
-	e.data.startsWith('{') && e.data.endsWith('}') ? clientMsg(data.data) : serverMessage(data);
-};
-
-const id = crypto.getRandomValues(new Uint16Array(1))[0];
-console.log('random id ', id);
-
 const sendBtn = document.querySelector('#send'),
 	closeBtn = document.querySelector('#close');
 
+const roomName = prompt('Enter room name').trim();
+
+const id = crypto.getRandomValues(new Uint16Array(1))[0],
+	exampSocket = new WebSocket('ws://buafacetime.herokuapp.com', 'echo-protocol');
+
 sendBtn.onclick = sendText;
 closeBtn.onclick = closeChat;
+exampSocket.onopen = openHandler;
+exampSocket.onmessage = messageHandler;
+
+const date = () => new Date(Date.now()).toDateString();
+const time = () => new Date(Date.now()).toLocaleTimeString();
+
+function closeChat(e) {
+	console.log("leaving chat.")
+	exampSocket.close(
+		3000,
+		JSON.stringify({
+			message: `${id}, left the chat room`,
+			roomName,
+		})
+	);
+}
+
+function openHandler(e) {
+	exampSocket.send(
+		JSON.stringify({
+			type: 'EnterRoom',
+			roomName,
+		})
+	);
+}
+
+function messageHandler(e) {
+	{
+		console.log('you got message....');
+
+		const data = JSON.parse(e.data);
+
+		console.log(data);
+		const clientMsg = (data) => {
+			let msg = document.createElement('div');
+			msg.textContent = `from :: ${data.id} \n ${data.text}`;
+			document.querySelector('#msgs').appendChild(msg);
+			return;
+		};
+
+		const serverMessage = (text) => {
+			const msg = document.createElement('div');
+			msg.textContent = `from server:: ${text}`;
+			document.querySelector('#msgs').appendChild(msg);
+			return;
+		};
+
+		e.data.startsWith('{') && e.data.endsWith('}') ? clientMsg(data) : serverMessage(data);
+	}
+}
 
 function sendText(e) {
-	console.log('send');
 	const msg = {
-		type: 'message',
-		text: document.getElementById('text').value,
 		id,
+		roomName,
 		date: date(),
 		time: time(),
-		name: "jive" //room name
+		type: 'message',
+		text: document.getElementById('text').value,
 	};
 
 	exampSocket.send(JSON.stringify(msg));
 	document.getElementById('text').value = '';
-}
-
-function date() {
-	return new Date(Date.now()).toDateString();
-}
-
-function time() {
-	return new Date(Date.now()).toLocaleTimeString();
-}
-
-function closeChat(e) {
-	exampSocket.close(3000,`${id}, left the chat room`);
 }
